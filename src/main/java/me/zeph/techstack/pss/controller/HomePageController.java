d4008a591e3e5348ff76ae1e0ae1138f1def7ecf
BenweiZhu <benw1988@yeah.net>
2013-09-03 19:04:55 +0800

    TechStackForPss [Ben] : integrate spring with tiles

--- src/main/java/me/zeph/techstack/pss/controller/HomePageController.java
+++ src/main/java/me/zeph/techstack/pss/controller/HomePageController.java
@@ -1,16 +1,14 @@
 package me.zeph.techstack.pss.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class HomePageController {
 	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
+	public String welcome() {
+		return "homepage";
 	}
 }
